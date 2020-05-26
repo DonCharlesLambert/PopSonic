1b3b50258267c045d70ac11afee0dac2bb5adb84
Don Charles-Lambert <zcabdc0@ucl.ac.uk>
2020-05-26 11:16:19 +0100

    Making this rn as I teach it

--- Sonic.py
+++ Sonic.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import time
 
 class Game():
     def __init__(self):
@@ -8,6 +9,7 @@ class Game():
         self.ring = Ring(self.canvas, 500)
         self.sonic = Sonic(self.canvas) #creates Sonic object and puts it on the canvas
         self.canvas.bind("<KeyPress>", self.key_press) # calls key_press function when btn is press
+        self.canvas.bind("<KeyRelease>", self.key_release)
         self.canvas.focus_set() # focuses button presses on the canvas
         self.canvas.pack() # packs the canvas into the window
         self.animation()
@@ -23,6 +25,9 @@ class Game():
         if key.char == "a":
             self.sonic.move("left")
 
+    def key_release(self, key):
+        self.sonic.stance()
+
     def animation(self):
         while True:
             if self.ring.pos()[0] - 10 < self.sonic.pos()[0] < self.ring.pos()[0] + 10:
@@ -31,35 +36,49 @@ class Game():
                     self.ring.move(200)
                 else:
                     self.ring.move(-200)
+            time.sleep(0.1)
+            self.sonic.anim()
             self.canvas.update()
                 
 
 class Sonic():
     animation = 0
-    RIGHT = [ "5",  "6",  "7",  "8",  "9", "10"]
-    LEFT  = ["20", "21", "22", "23", "24", "25"]
+    direction = "right"
+    action = "STANCE"
+    STANCE_LEFT  = ["15", "16", "17", "18", "19"]
+    STANCE_RIGHT = ["0", "1", "2", "3", "4"]
+    RUN_RIGHT    = [ "5",  "6",  "7",  "8",  "9", "10"]
+    RUN_LEFT     = ["20", "21", "22", "23", "24", "25"]
     def __init__(self, canvas):
         self.sprite = PhotoImage(file="Sonic/0.png") # gets the sonic image
         self.sonic = canvas.create_image(300, 330, image=self.sprite) # puts sonic image on canvas
         self.canvas = canvas
-        
+
+    def stance(self):
+        self.action = "STANCE"
+    
     def move(self, direction):
+        self.action = "RUN"
+        self.direction = direction
         if direction == "right":
             self.canvas.move(self.sonic, 10, 0)
         elif direction == "left":
             self.canvas.move(self.sonic, -10, 0)
-        self.anim(direction)
 
     def pos(self):
         return self.canvas.coords(self.sonic)
 
-    def anim(self, direction):
-        sprite_list = ["0"]
-        if direction == "right":
-            sprite_list = self.RIGHT
-        else:
-            sprite_list = self.LEFT
-
+    def anim(self):
+        direction = self.direction
+        if direction == "right" and self.action == "RUN":
+            sprite_list = self.RUN_RIGHT
+        elif direction == "left" and self.action == "RUN":
+            sprite_list = self.RUN_LEFT
+        elif direction == "right" and self.action == "STANCE":
+            sprite_list = self.STANCE_RIGHT
+        elif direction == "left" and self.action == "STANCE":
+            sprite_list = self.STANCE_LEFT
+        
         self.animation = (self.animation + 1) % len(sprite_list)    
         self.sprite = PhotoImage(file=r"Sonic/" + sprite_list[self.animation] + ".png")
         self.canvas.itemconfig(self.sonic, image=self.sprite)
